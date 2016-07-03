3d7af38d983cfac0654d1462278b53ac6fd0334d
Jakub Rohla <freemanovec@gmail.com>
2016-07-03 08:24:28 +0200

    Added proxy init callings hooks

--- src/main/java/dustenricher/common/Main.java
+++ src/main/java/dustenricher/common/Main.java
@@ -4,6 +4,8 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import dustenricher.blocks.Blank;
 import net.minecraft.block.Block;
@@ -17,9 +19,19 @@ public class Main {
 	public static CommonProxy proxy;
 	
 	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		proxy.preInit(event);
+	}
+	@EventHandler
 	public void init(FMLInitializationEvent event){
+		proxy.init(event);
+		
 		initItems();
 		initBlocks();
+	}
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		proxy.postInit(event);
 	}
 	void initBlocks(){
 		Block dustInjectionChamber = new Blank(Material.iron).setBlockName("dustInjectionChamber");
